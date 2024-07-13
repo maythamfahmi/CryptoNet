4da7f3affca8df07e45c4c94319b80a4b67b077c
Maytham Fahmi <9260645+maythamfahmi@users.noreply.github.com>
2024-07-13 23:42:03 +0200

    Introduce styled cli for testing results via CLI

---
+++ CryptoNet.Cli/DebugExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CryptoNet.Cli
+{
+    public static class Debug
+    {
+        public static void Assert(bool condition, [CallerArgumentExpression(nameof(condition))] string message = "")
+        {
+            if (condition)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Assert passed: {message}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Assert failed: {message}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
