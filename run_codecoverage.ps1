ae5e1d1de9a3de13323ed8d54fe6261e0e163777
Maytham Fahmi <9260645+maythamfahmi@users.noreply.github.com>
2024-07-16 21:44:40 +0200

    add code coverage cli and doc

---
+++ run_codecoverage.ps1
@@ -0,0 +1,11 @@
+#dotnet test --collect:"XPlat Code Coverage"
+#reportgenerator -reports:"./CryptoNet.UnitTests/TestResults/*/coverage.cobertura.xml" -targetdir:"./CoverageReport" -reporttypes:"Html;Badges"
+
+$Url = "$(Get-Location)\CoverageReport\index.html"
+$ProcId = (Get-Process -Name '*edge*' | Where-Object MainWindowTitle -like "*Summary - Coverage Report*").Id
+
+if ($ProcId) {
+    Stop-Process -Id $ProcId
+}
+
+Start-Process msedge $($Url)
