1344ae2daeef44ccbeb586b35f0d09d2b5996aa2
Maytham Fahmi <9260645+maythamfahmi@users.noreply.github.com>
2024-11-03 09:51:13 +0100

    #104 improve run docs

--- run_docs.ps1
+++ run_docs.ps1
@@ -1,10 +1,15 @@
-## Once
-# dotnet tool install -g docfx      // install this tool once.
-# docfx init -y                     // already done and configured once.
-
-## Each time
 Remove-Item .\api -Recurse
 Remove-Item .\_site\. -Recurse
 docfx metadata
 docfx build .\docfx.json 
+
+$Url = "http://localhost:8080"
+$ProcId = (Get-Process -Name '*edge*' | Where-Object MainWindowTitle -like "*Summary - Coverage Report*").Id
+
+if ($ProcId) {
+    Stop-Process -Id $ProcId
+}
+
+Start-Process msedge $($Url)
+
 docfx serve _site
